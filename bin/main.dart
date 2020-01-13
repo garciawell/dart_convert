bdde5600e68de9777e668bc5a884d23e5a39cd52
Wellington Garcia <garciawell@gmail.com>
2020-01-12 22:06:24 -0300

    Creating function to get all images

--- bin/main.dart
+++ bin/main.dart
@@ -9,4 +9,27 @@ void main() {
   if (!choice) {
     exit(0);
   }
+
+  List<Option> buildFormatOptions() {
+    return [
+      new Option('Convert to jpeg', 'jpeg'),
+      new Option('Convert to png', 'png')
+    ];
+  }
+
+  List<Option> buildFileOptions() {
+    return Directory.current.listSync().where(
+      (entity) {
+        return FileSystemEntity.isFileSync(entity.path) &&
+            entity.path.contains(new RegExp(r'\.(png|jpg|jpeg)'));
+      },
+    ).map((entity) {
+      final filename = entity.path.split(Platform.pathSeparator).last;
+      return new Option(filename, entity);
+    }).toList();
+  }
+
+  final format = prompter.askMultiple('Select format:', buildFormatOptions());
+  // prompter.askMultiple('Select an image to convert:', buildFileOptions());
+  buildFileOptions();
 }
